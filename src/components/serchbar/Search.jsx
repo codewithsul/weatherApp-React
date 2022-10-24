8e70211cb557dcb125decc7f4d7231124f20ff6a
codewithsul <suleiman4a@gmail.com>
2022-10-24 06:41:03 -0700

    weatherapp

--- src/components/serchbar/Search.jsx
+++ src/components/serchbar/Search.jsx
@@ -1,16 +1,73 @@
 import React from "react";
 import "./search.css";
 import Weather from "../weatherdata/Weather";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
-const search = () => {
+const Search = () => {
+  const [Temp, setTemp] = useState("");
+  const [speed, setSpeed] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [humi, setHumi] = useState("");
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState([]);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=620219b682a4e7d867d69c80f5631303`;
+
+  const handleChange = (event) => {
+    const city = event.target.value;
+    setLocation(city);
+  };
+
+  const GetWeatherData = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        const weatherinfo = response.data;
+        const { temp } = response.data.main;
+        const { pressure } = response.data.main;
+        const { speed } = response.data.wind;
+        const { humidity } = response.data.main;
+        const { name } = response.data;
+        console.log(weatherinfo);
+        setTemp(temp);
+        setSpeed(speed);
+        setPressure(pressure);
+        setHumi(humidity);
+        setName(name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className="field">
-        <input id="textfield" type="text" placeholder="Enter City Name"></input>
+        <input
+          onChange={handleChange}
+          id="textfield"
+          type="text"
+          placeholder="Enter City Name"
+        ></input>
       </div>
-      <Weather />
+      <div className="button">
+        <button className="btn" onClick={GetWeatherData}>
+          Request
+        </button>
+      </div>
+
+      <Weather
+        temp={Temp}
+        pressure={pressure}
+        speed={speed}
+        humidity={humi}
+        name={name}
+        location={location}
+      />
     </>
   );
 };
 
-export default search;
+export default Search;
--- src/components/weatherdata/Weather.jsx
+++ src/components/weatherdata/Weather.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import "./weather.css";
 
-const weather = () => {
+const weather = (props) => {
   return (
-    <div className="weatherwrap">
-      <div className="Temp">
-        <h6>Temperature</h6>
-        <h5>65 &#176; F</h5>
+    <>
+      <div className="name">{props.location}</div>
+      <div className="weatherwrap">
+        <div className="Temp">
+          <h6>Temperature</h6>
+          <h5>{props.temp}K</h5>
+        </div>
+        <div className="sky">
+          <h6>Pressure</h6>
+          <h5>{props.pressure}hPa</h5>
+        </div>
+        <div className="Humidity">
+          <h6>Humidity</h6>
+          <h5>{props.humidity}%</h5>
+        </div>
+        <div className="Wind">
+          <h6>Wind Speed</h6>
+          <h5>{props.speed}m/s</h5>
+        </div>
       </div>
-      <div className="sky">
-        <h6>Clear</h6>
-      </div>
-      <div className="Humidity">
-        <h6>Humidity</h6>
-      </div>
-      <div className="Wind">
-        <h6>Wind</h6>
-      </div>
-    </div>
+      <div className="foot">codewithsul|2022</div>
+    </>
   );
 };
 
